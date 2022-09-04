d5f776c70695548303fa5f96dc873c178fca4c96
Swayam <95397970+schlechter-afk@users.noreply.github.com>
2022-09-04 10:23:12 +0530

    Update ls.c

--- ls.c
+++ ls.c
@@ -18,12 +18,13 @@ int default_filter2(const struct dirent *ent)
 
 void lscmd(char *argumentList[50], int len)
 {
+    // printf("%s\n", argumentList[1]);
+
     setlocale(LC_ALL, "");
     if (len == 1 || strcmp(argumentList[1], ".") == 0)
     {
-        int arg;
         struct dirent **list = NULL;
-        int i, count;
+        int count;
         char *currdirft = (char *)malloc(sizeof(char) * 100);
         getcwd(currdirft, 1000);
 
@@ -35,39 +36,144 @@ void lscmd(char *argumentList[50], int len)
             return;
         }
 
-        for (i = 0; i < count; i++)
+        for (int i = 0; i < count; i++)
         {
             printf("%s\n", list[i]->d_name);
         }
     }
     else
     {
-        if ((strcmp(argumentList[1], "-a") == 0) || (strcmp(argumentList[1], "--all") == 0))
+        if (strcmp(argumentList[1], "-a") == 0)
         {
-            int arg;
-            struct dirent **list = NULL;
-            int i, count;
-            char *currdirft = (char *)malloc(sizeof(char) * 100);
-            getcwd(currdirft, 1000);
-
-            count = scandir(currdirft, &list, default_filter2, alphasort);
-
-            if (count == -1)
+            if (strcmp(argumentList[2], "-l") == 0)
             {
-                printf("ls: cannot access the given input: No such file or directory");
+                // printf("mai yaha aagaya\n");
+                struct dirent **list = NULL;
+                char *currdirft = (char *)malloc(sizeof(char) * 100);
+                getcwd(currdirft, 1000);
+
+                int count = scandir(currdirft, &list, default_filter2, alphasort);
+
+                if (count == -1)
+                {
+                    printf("ls: cannot access the given input: No such file or directory");
+                    return;
+                }
+
+                long long int total = 0;
+                struct stat stats;
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (stat(list[i]->d_name, &stats) == 0)
+                        total += stats.st_blocks;
+                }
+
+                printf("total %lld\n", total / 2);
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (stat(list[i]->d_name, &stats) == 0)
+                    {
+                        if (S_ISDIR(stats.st_mode))
+                            printf("d");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IRUSR)
+                            printf("r");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IWUSR)
+                            printf("w");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IXUSR)
+                            printf("x");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IRGRP)
+                            printf("r");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IWGRP)
+                            printf("w");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IXGRP)
+                            printf("x");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IROTH)
+                            printf("r");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IWOTH)
+                            printf("w");
+                        else
+                            printf("-");
+
+                        if (stats.st_mode & S_IXOTH)
+                            printf("x");
+                        else
+                            printf("-");
+
+                        printf("%ld ", stats.st_nlink);                   // Number of hard links
+                        printf("%s ", (getpwuid(stats.st_uid))->pw_name); // owner name
+                        printf("%s ", (getgrgid(stats.st_gid))->gr_name); // group name
+
+                        printf("%ld ", stats.st_size); // Total size, in bytes
+                        char last_mod_time[16];
+                        strftime(last_mod_time, 14, "%h %d %H:%M", localtime(&stats.st_mtime)); // Time of last modification
+                        printf("%s ", last_mod_time);
+
+                        printf("%s\n", list[i]->d_name);
+                    } // end of if
+                }     // end of for
+                for (int i = 0; i < len; i++)
+                {
+                    argumentList[i][0] = '\0';
+                }
                 return;
-            }
-
-            for (i = 0; i < count; i++)
+            } // end of big if
+            else
             {
-                printf("%s\n", list[i]->d_name);
+                // printf("aya yaha pe finally\n");
+                struct dirent **list = NULL;
+                int count;
+                char *currdirft = (char *)malloc(sizeof(char) * 100);
+                getcwd(currdirft, 1000);
+
+                count = scandir(currdirft, &list, default_filter2, alphasort);
+
+                if (count == -1)
+                {
+                    printf("ls: cannot access the given input: No such file or directory");
+                    return;
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    printf("%s\n", list[i]->d_name);
+                }
+                for (int i = 0; i < len; i++)
+                {
+                    argumentList[i][0] = '\0';
+                }
+                return;
             }
         }
         else if ((strcmp(argumentList[1], "..") == 0))
         {
-            int arg;
             struct dirent **list = NULL;
-            int i, count;
+            int count;
             char *currdirft = (char *)malloc(sizeof(char) * 100);
             getcwd(currdirft, 1000);
 
@@ -102,16 +208,20 @@ void lscmd(char *argumentList[50], int len)
                 return;
             }
 
-            for (i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
             {
                 printf("%s\n", list[i]->d_name);
             }
+            for (int i = 0; i < len; i++)
+            {
+                argumentList[i][0] = '\0';
+            }
+            return;
         }
         else if (argumentList[1][0] == '~')
         {
-            int arg;
             struct dirent **list = NULL;
-            int i, count;
+            int count;
 
             char *chktilda = (char *)malloc(sizeof(char *) * 200);
             strcpy(chktilda, homedir);
@@ -126,14 +236,237 @@ void lscmd(char *argumentList[50], int len)
                 return;
             }
 
-            for (i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
             {
                 printf("%s\n", list[i]->d_name);
             }
+            for (int i = 0; i < len; i++)
+            {
+                argumentList[i][0] = '\0';
+            }
+            return;
         }
         else if ((strcmp(argumentList[1], "-l") == 0))
         {
-            
+            struct dirent **list = NULL;
+            char *currdirft = (char *)malloc(sizeof(char) * 100);
+            getcwd(currdirft, 1000);
+
+            int count = scandir(currdirft, &list, default_filter, alphasort);
+
+            if (count == -1)
+            {
+                printf("ls: cannot access the given input: No such file or directory");
+                return;
+            }
+
+            long long int total = 0;
+            struct stat stats;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (stat(list[i]->d_name, &stats) == 0)
+                    total += stats.st_blocks;
+            }
+
+            printf("total %lld\n", total / 2);
+
+            for (int i = 0; i < count; i++)
+            {
+                if (stat(list[i]->d_name, &stats) == 0)
+                {
+                    if (S_ISDIR(stats.st_mode))
+                        printf("d");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IRUSR)
+                        printf("r");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IWUSR)
+                        printf("w");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IXUSR)
+                        printf("x");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IRGRP)
+                        printf("r");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IWGRP)
+                        printf("w");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IXGRP)
+                        printf("x");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IROTH)
+                        printf("r");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IWOTH)
+                        printf("w");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IXOTH)
+                        printf("x");
+                    else
+                        printf("-");
+
+                    printf("%ld ", stats.st_nlink);                   // Number of hard links
+                    printf("%s ", (getpwuid(stats.st_uid))->pw_name); // owner name
+                    printf("%s ", (getgrgid(stats.st_gid))->gr_name); // group name
+
+                    printf("%ld ", stats.st_size); // Total size, in bytes
+                    char last_mod_time[16];
+                    strftime(last_mod_time, 14, "%h %d %H:%M", localtime(&stats.st_mtime)); // Time of last modification
+                    printf("%s ", last_mod_time);
+
+                    printf("%s\n", list[i]->d_name);
+                }
+            }
+            for (int i = 0; i < len; i++)
+            {
+                argumentList[i][0] = '\0';
+            }
+            return;
+        }
+        else if ((strcmp(argumentList[1], "-al") == 0) || ((strcmp(argumentList[1], "-la") == 0)))
+        {
+            struct dirent **list = NULL;
+            char *currdirft = (char *)malloc(sizeof(char) * 100);
+            getcwd(currdirft, 1000);
+
+            int count = scandir(currdirft, &list, default_filter2, alphasort);
+
+            if (count == -1)
+            {
+                printf("ls: cannot access the given input: No such file or directory");
+                return;
+            }
+
+            long long int total = 0;
+            struct stat stats;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (stat(list[i]->d_name, &stats) == 0)
+                    total += stats.st_blocks;
+            }
+
+            printf("total %lld\n", total / 2);
+
+            for (int i = 0; i < count; i++)
+            {
+                if (stat(list[i]->d_name, &stats) == 0)
+                {
+                    if (S_ISDIR(stats.st_mode))
+                        printf("d");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IRUSR)
+                        printf("r");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IWUSR)
+                        printf("w");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IXUSR)
+                        printf("x");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IRGRP)
+                        printf("r");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IWGRP)
+                        printf("w");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IXGRP)
+                        printf("x");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IROTH)
+                        printf("r");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IWOTH)
+                        printf("w");
+                    else
+                        printf("-");
+
+                    if (stats.st_mode & S_IXOTH)
+                        printf("x");
+                    else
+                        printf("-");
+
+                    printf("%ld ", stats.st_nlink);                   // Number of hard links
+                    printf("%s ", (getpwuid(stats.st_uid))->pw_name); // owner name
+                    printf("%s ", (getgrgid(stats.st_gid))->gr_name); // group name
+
+                    printf("%ld ", stats.st_size); // Total size, in bytes
+                    char last_mod_time[16];
+                    strftime(last_mod_time, 14, "%h %d %H:%M", localtime(&stats.st_mtime)); // Time of last modification
+                    printf("%s ", last_mod_time);
+
+                    printf("%s\n", list[i]->d_name);
+                }
+            }
+            for (int i = 0; i < len; i++)
+            {
+                argumentList[i][0] = '\0';
+            }
+            return;
+        }
+        else
+        {
+            if (!access(argumentList[1], F_OK))
+            {
+                struct dirent **list = NULL;
+                int count;
+                count = scandir(argumentList[1], &list, default_filter, alphasort);
+
+                if (count == -1)
+                {
+                    printf("%s\n", argumentList[1]);
+                    return;
+                }
+                else
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        printf("%s\n", list[i]->d_name);
+                    }
+                }
+                return;
+            }
+            else
+            {
+                printf("ls: cannot access '%s': No such file or directory\n",argumentList[1]);
+                return;
+            }
         }
     }
     return;
